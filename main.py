e9fb9ffff4ed413dd877e8cdd3922b21ef04f2ba
CODETECHOFFICIAL <87864573+CODETECHOFFICIAL@users.noreply.github.com>
2021-07-23 23:06:28 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login('mihir.patilmihir14@gmail.com', 'Mihir@119')
-server.sendmail('mihir.patilmihir14@gmail.com',
-                'mihir.patilmihir598@gmail.com',
-                'Hi , thanks for appreciating')
